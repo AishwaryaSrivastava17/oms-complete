0ea4eb454f74476a628bb4d5e516d758f5ac1383
Aishwarya Srivastava <aishwaryaaaaaaaaaaa@gmail.com>
2021-03-29 16:45:23 +0530

    bring all code in single place

---
+++ demo/src/main/java/com/ibm/demo/OrderController.java
@@ -0,0 +1,72 @@
+package com.ibm.demo;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid; 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ibm.demo.entity.Order;
+import com.ibm.demo.service.OrderService;
+
+
+
+@RestController //Bean
+public class OrderController { //frontend
+	@Autowired //is used for DI
+	OrderService orderService;   // DI
+	@PostMapping("/order")
+	@ResponseStatus(code = HttpStatus.CREATED)
+	String createOrder(@RequestBody @Valid Order order, BindingResult bindingResult) {
+		validateModel(bindingResult);
+		System.out.println(order);
+//		return orderService.createOrder(order);
+		return orderService.createOrder(order); //delegate 
+	}
+
+	@GetMapping("/order")
+	List<Order> getOrders(){
+		return orderService.getOrders();
+	}
+	@GetMapping("/order/{id}")
+	Optional<Order> getOrder(@PathVariable("id")String orderId) {
+		return orderService.getOrder(orderId);
+	}
+//	String getOrder() {
+//		return "order created";
+//		return orderService.getOrder();
+	private void validateModel(Errors bindingResult) {
+		if(bindingResult.hasErrors()) {
+			throw new IllegalArgumentException("Something went wrong. Plesae retry");
+		}
+	}
+
+	@PutMapping("/order/{id}")
+	void updateOrder(@RequestBody @Valid Order order,BindingResult bindingResult,@PathVariable("id") String orderId) {
+		validateModel(bindingResult);
+		System.out.println(orderId);
+		order.setId(orderId);
+		orderService.updateOrder(order);
+		//return "order updated";
+	}
+	@DeleteMapping("/order/{id}")
+	void deleteOrder(@PathVariable("id") String deleteId) {
+		
+//		System.out.println(httpRequest.getHeaders());
+		System.out.println(deleteId);
+	orderService.deleteOrder(deleteId);	
+//		return "order deleted";
+	}
+}
